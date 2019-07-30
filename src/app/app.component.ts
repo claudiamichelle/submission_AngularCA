0700d371554e0c46d803fd3b0baafd52d133f669
Claudia Michelle Paramesvari <claudia_michelle@outlook.com>
2019-07-30 19:52:20 +0800

    details showing url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular CA';
+  title = 'Angular CA - Starwars (ISS SA47)';
 }
 
 
--- src/app/people-detail/people-detail.component.ts
+++ src/app/people-detail/people-detail.component.ts
@@ -1,17 +1,15 @@
-
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 import {PeopleService} from "../people.service";
 import {ActivatedRoute} from "@angular/router";
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './people-detail.component.html',
-  styleUrls: [ './people-detail.component.css' ]
+    selector: 'app-hero-detail',
+    templateUrl: './people-detail.component.html',
+    styleUrls: ['./people-detail.component.css']
 })
 export class PeopleDetailComponent implements OnInit {
     @Input()
-
     peoples: any[];
     nextPg: string;
     thisPg: string;
@@ -21,27 +19,34 @@ export class PeopleDetailComponent implements OnInit {
     constructor(
         private route: ActivatedRoute,
         private peopleService: PeopleService,
-    ){}
+
+    ) {
+    }
 
     ngOnInit(): void {
         this.thisPg = 'https://swapi.co/api/people/';
         this.prevPg = '';
         this.nextPg = '';
         this.getPeople();
+
     }
 
     getPeople(): void {
 
         this.peopleService.callAPI(this.thisPg).subscribe(
             peoples => {
-                this.peoples   = Object.values(peoples['results']);
+                this.peoples = Object.values(peoples['results']);
             })
 
         this.peopleService.callAPI(this.thisPg).subscribe(
-            prevPg => {this.prevPg = Object.values(prevPg['previous']).join("");});
+            prevPg => {
+                this.prevPg = Object.values(prevPg['previous']).join("");
+            });
 
         this.peopleService.callAPI(this.thisPg).subscribe(
-            nextPg => {this.nextPg = Object.values(nextPg['next']).join("");});
+            nextPg => {
+                this.nextPg = Object.values(nextPg['next']).join("");
+            });
 
         console.log('Current Pg : ' + this.thisPg);
         console.log('Previous Pg: ' + this.prevPg);
@@ -54,7 +59,9 @@ export class PeopleDetailComponent implements OnInit {
         this.prevPg = this.thisPg;
         this.thisPg = this.nextPg;
         this.peopleService.callAPI(this.nextPg).subscribe(
-            nextPg => {this.nextPg = Object.values(nextPg['next']).join("");});
+            nextPg => {
+                this.nextPg = Object.values(nextPg['next']).join("");
+            });
 
 
         console.log('Current Pg : ' + this.thisPg);
@@ -80,7 +87,9 @@ export class PeopleDetailComponent implements OnInit {
         this.nextPg = this.thisPg;
         this.thisPg = this.prevPg;
         this.peopleService.callAPI(this.prevPg).subscribe(
-            prevPg => {this.prevPg = Object.values(prevPg['previous']).join("");});
+            prevPg => {
+                this.prevPg = Object.values(prevPg['previous']).join("");
+            });
 
         console.log('Current Pg: ' + this.thisPg);
         console.log('Previous Pg: ' + this.prevPg);
@@ -92,11 +101,12 @@ export class PeopleDetailComponent implements OnInit {
             });
     }
 
-    getPeopleDetail(number :number): void {
+    getPeopleDetail(number: number): void {
         console.log('CLICKED A PERSON');
         console.log(number);
         this.peopleService.getPeopleDetails(number).subscribe(details => this.details = Object(details));
     }
+
 }
 
 
